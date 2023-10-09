9c58e612f0f3b2563ca5c9f4b68cbc9a2182f4f6
xkolsha <aviad85@outlook.com>
2023-10-08 21:16:05 -0300

    fixed liks

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -6,8 +6,8 @@ import atlanticImage from "../assets/images/Atlantic.jpeg";
 import fontasticImage from "../assets/images/Fontastic.jpeg";
 import educationImage from "../assets/images/Education.jpg";
 import gmsImage from "../assets/images/GMS.jpg";
-import harmonyImage from "../assets/images/harmony.png";
-import beaconImage from "../assets/images/beacon.jpg";
+import harmonyImage from "../assets/images/Harmony.jpg";
+import beaconImage from "../assets/images/Beacon.jpg";
 
 const Portfolio = () => {
   const projects = [
