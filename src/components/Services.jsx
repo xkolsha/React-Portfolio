7c40f009bf88c39a05560f170eabc5e64912efbe
xkolsha <aviad85@outlook.com>
2023-10-08 22:39:50 -0300

    added start script

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -15,19 +15,19 @@ const services = [
     animationData: animationData1,
   },
   {
-    title: "Graphic Design",
+    title: "Graphic Design & Visual Identity",
     description:
       "Every business has a unique identity. I will work closely with you to create a visual identity that perfectly represents your brand. I design original logos, business cards, envelopes, catalogues, labels, brochures, posters, calendars, and more. From concept to completion, I will ensure that your design stands out and effectively showcases your brand. Don't settle for generic designs. Contact me today to create your business's unique and memorable visual identity.",
     animationData: animationData2,
   },
   {
-    title: "Branding",
+    title: "BBranding & Strategy",
     description:
       "Investing in your business branding is how you outperform and outlast your competitors. The goal is to attract and retain loyal customers by delivering a product that always aligns with the brand's promises. Keep your competition from getting ahead - Contact me today and let me help you create a solid and consistent brand that will set you apart from the rest.",
     animationData: animationData3,
   },
   {
-    title: "Video Editing",
+    title: "Video Editing & Visual Storytelling",
     description:
       "Elevate your visual content with my professional video editing services. I'll collaborate with you to bring your film or corporate video ideas to life, offering services such as colour correction, sound design, motion graphics, and more. Contact me to deliver engaging, high-quality videos that will leave a lasting impression on your audience.",
     animationData: animationData4,
