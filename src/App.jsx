f9857dd6d5c8f359d6c6192dddd428028c9ea0b5
xkolsha <aviad85@outlook.com>
2023-11-18 17:49:08 -0400

    refactor the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useRef } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import { ThemeProvider } from "@mui/material/styles";
+import { ThemeProvider, Box } from "@mui/material";
 import Navigation from "./components/Navigation";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
@@ -8,7 +8,7 @@ import About from "./components/About";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import theme from "./theme";
+import theme from "./theme"; // Ensure the correct path to your theme file
 
 function App() {
   const heroRef = useRef(null);
@@ -20,33 +20,49 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <Router>
-        <Navigation
-          heroRef={heroRef}
-          servicesRef={servicesRef}
-          aboutRef={aboutRef}
-          portfolioRef={portfolioRef}
-          contactRef={contactRef}
-        />
-        <div style={{ paddingTop: "64px" }}>
-          <Hero portfolioRef={portfolioRef} contactRef={contactRef} />
-          <div ref={servicesRef}>
-            <Services />
-          </div>{" "}
-          {/* Services Section */}
-          <div ref={aboutRef}>
-            <About />
-          </div>{" "}
-          {/* About Section */}
-          <div ref={portfolioRef}>
-            <Portfolio />
+        <Box sx={{ bgcolor: "background.default", minHeight: "100vh" }}>
+          {/* Navigation bar component with refs passed for smooth scrolling */}
+          <Navigation
+            heroRef={heroRef}
+            servicesRef={servicesRef}
+            aboutRef={aboutRef}
+            portfolioRef={portfolioRef}
+            contactRef={contactRef}
+          />
+
+          {/* Main content area */}
+          <div style={{ paddingTop: "64px" }}>
+            {/* Hero section with refs for scrolling */}
+            <Hero
+              id="hero"
+              portfolioRef={portfolioRef}
+              contactRef={contactRef}
+            />
+
+            {/* Services section */}
+            <div ref={servicesRef}>
+              <Services />
+            </div>
+
+            {/* About section */}
+            <div id="about" ref={aboutRef}>
+              <About />
+            </div>
+
+            {/* Portfolio section */}
+            <div id="portfolio" ref={portfolioRef}>
+              <Portfolio />
+            </div>
+
+            {/* Contact section */}
+            <div id="contact" ref={contactRef}>
+              <Contact />
+            </div>
           </div>
-          {/* Portfolio Section */}
-          <div ref={contactRef}>
-            <Contact />
-          </div>
-          {/* Contact Section */}
-        </div>
-        <Footer /> {/* Footer Section */}
+
+          {/* Footer component */}
+          <Footer />
+        </Box>
       </Router>
     </ThemeProvider>
   );
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { useState } from "react";
+import { Link /* useLocation */ } from "react-router-dom";
+import PropTypes from "prop-types";
 import {
   AppBar,
   Toolbar,
@@ -15,44 +16,50 @@ import {
 import MenuIcon from "@mui/icons-material/Menu";
 import logo from "../assets/images/nav-logo.png";
 
-const Navigation = ({
-  heroRef,
-  servicesRef,
-  aboutRef,
-  portfolioRef,
-  contactRef,
-}) => {
+// Navigation component with props for different section refs
+const Navigation = ({ servicesRef, aboutRef, portfolioRef, contactRef }) => {
+  // State for managing mobile menu drawer open/close
   const [menuOpen, setMenuOpen] = useState(false);
-  const location = useLocation();
+  //TODO - Getting the current location for active link styling
+  // const location = useLocation();
 
+  // Define navigation items, each with a name, path, and ref
   const navItems = [
-    { name: "Home", path: "/", ref: heroRef },
+    { name: "Home", path: "/", ref: null },
     { name: "Services", path: "/services", ref: servicesRef },
     { name: "About", path: "/about", ref: aboutRef },
     { name: "Portfolio", path: "/portfolio", ref: portfolioRef },
     { name: "Contact", path: "/contact", ref: contactRef },
   ];
 
-  const isActive = (path) => (location.pathname === path ? "active" : "");
+  //TODO - Function to determine if a nav item is active
+  // const isActive = (path) => (location.pathname === path ? "active" : "");
 
+  // Function to handle scrolling to a section
   const scrollToSection = (ref) => {
     if (ref && ref.current) {
       ref.current.scrollIntoView({ behavior: "smooth" });
+    } else if (ref === null) {
+      // For Home button, scroll to top of the page
+      window.scrollTo({ top: 0, behavior: "smooth" });
     }
     setMenuOpen(false); // Close the drawer when a menu item is clicked
   };
 
   return (
-    <AppBar position="fixed" color="default">
+    <AppBar
+      position="fixed"
+      color="default"
+      sx={{ bgcolor: "background.default" }}
+    >
       <Toolbar>
-        {/* Logo */}
+        {/*Logo with onClick handler */}
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          <Link to="/" aria-label="Home">
+          <Link to="/" aria-label="Home" onClick={() => scrollToSection(null)}>
             <img src={logo} alt="logo" style={{ height: "40px" }} />
           </Link>
         </Typography>
-
-        {/* Hamburger Menu Icon */}
+        {/* Hamburger Menu Icon for Mobile Navigation */}
         <IconButton
           color="inherit"
           edge="start"
@@ -62,7 +69,7 @@ const Navigation = ({
           <MenuIcon />
         </IconButton>
 
-        {/* Drawer for Mobile Navigation */}
+        {/* Drawer Component for Mobile Navigation */}
         <Drawer
           anchor="right"
           open={menuOpen}
@@ -71,11 +78,7 @@ const Navigation = ({
           <Box sx={{ width: 250 }} role="presentation">
             <List>
               {navItems.map((item, index) => (
-                <ListItem
-                  button
-                  key={index}
-                  onClick={() => scrollToSection(item.ref)}
-                >
+                <ListItem key={index} onClick={() => scrollToSection(item.ref)}>
                   <ListItemText primary={item.name} />
                 </ListItem>
               ))}
@@ -83,7 +86,7 @@ const Navigation = ({
           </Box>
         </Drawer>
 
-        {/* Center Links for Large Screens */}
+        {/* Navigation Links for Larger Screens */}
         <Box
           sx={{
             flexGrow: 1,
@@ -104,6 +107,26 @@ const Navigation = ({
       </Toolbar>
     </AppBar>
   );
+};
+
+// PropType validation for all refs passed to the component
+Navigation.propTypes = {
+  servicesRef: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.shape({ current: PropTypes.any }),
+  ]),
+  aboutRef: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.shape({ current: PropTypes.any }),
+  ]),
+  portfolioRef: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.shape({ current: PropTypes.any }),
+  ]),
+  contactRef: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.shape({ current: PropTypes.any }),
+  ]),
 };
 
 export default Navigation;
